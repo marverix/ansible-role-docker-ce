55ab852de93981cecbaf6f240820d37840a1382e
Marek Sierociński <mareksierocinski@gmail.com>
2021-04-10 21:10:07 +0200

    Moving test.sh to Travis script section

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-molecule --base-config molecule/base.yml test --scenario-name default
-molecule --base-config molecule/base.yml test --scenario-name without-sdk
-molecule --base-config molecule/base.yml test --scenario-name without-compose
